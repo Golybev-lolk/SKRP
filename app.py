2d5e7cf1639ec115f0a6e3211c04b8113f0f9e55
Golybev-lolk <poskopsossas@mail.ru>
2026-08-29 19:32:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,63 +6,19 @@ from google.oauth2.service_account import Credentials
 # Точный ID вашей Google Таблицы
 SPREADSHEET_ID = "13H-fmBuw2vpzsB5ci6oPzl26-_nZ7LRkhZVpJIG81Uo"
 
-# Безопасная запись приватного ключа списком строк
-KEY_LINES = [
-    "-----BEGIN PRIVATE KEY-----\n",
-    "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKgwggSjAgEAAoIBAQDHRDMSz5TWhTf2\n",
-    "msEpSBgvj3hRE1eqLBMYg7sceUb5FjV5hYn1NS7dsYwXlqgMYGbAIWvzK9TZAeQD\n",
-    "O+HxikwArNKG43LUwZ1KNQlCIybpLKY4bmvwSuAzZFO7igIvu4+E8HXzVsOwIaLa\n"
-    "EANxwdVaH8Xftr66reIgXeUmFdcl2Y6xt/aj2NJKeGekH0WtNyle1QYJbk1eCgSR\n",
-    "8FjnCfyk/1+HIpjpa3sf74+uvOpY5p9m13qaQffU7lne/NInFfi7rL+2T5z5LUps\n",
-    "y0pPJpC8QakPkBFk+NwtxpzddmzvIj5aGAuZweeO1wDI5TbneVObzN3MetFMrFqN\n",
-    "vJx9xjp9AgMBAAECggEAF0/6Bq8c610CD1qLEyyRUc0f5yJTbQADiuhsdVgV7tHD\n",
-    "l1BBc9vRPBQHvwG77ti/ZKZ0VepwmCPRvHQeyq6iliih57CH8G6nW3VpfCCXLovv\n",
-    "9WWrwJzN7o1EA4qRl2qCz6t/LSUfN0W0Iv1hHceu0YCqmnHrjfUGYcGCa8p91XpR\n",
-    "xL4avE9Xs+npkaOVXNprrGLw2OA3r3n63U3gAyQqyC6MgVG8yoBv2470Z+upcnzU\n",
-    "23CsbaTqKY+12JXpLS4wB79p80iZ11S2ngXr3lYZbw7YRoPi3NGBoubS5qTyXbn5\n",
-    "KiEXNSwfpbt7winbDxojmwS2kdfxbkKncZwum0JzqQKBgQDrGVUZTvGhkSLyQRol\n",
-    "UbVpT3zThx77JahBvnmVBFh6WJnQHfbJshj2yL4Thg0ZBbBnKDvjJ6e+izlAufSB\n",
-    "BY8OFuLUd6lk454spXKYG202cxhOmPQNBpCTn9HWjfFn7hkGVGrcMyEtmqXvTXdq\n",
-    "EeXl7uDcRcecZ8CdnGD7N9+w6QKBgQDY+1isYvcqmvI6S5nfj3EjcI95Eu5d0x/7\n"
-    "bVyHGcmGwshASs3mXPxP1kCNiJoi1rZlYIm/68JBSQs8aPYx2/25NJiUlgA9R0gM\n"
-    "0VHwQTsZykZh0mXqJtpTAh3zGChj8pNgeB/qFduSXjJWXdna/hYnuqaWeLfSYdm1\n"
-    "rAN8GLtgdQKBgAmDcOohQ63iDaP39nLkJ48nZEm7AgnvsCtY5dqd3ma1ByRg0ge/\n"
-    "aTszTF3F4unMTSMfgazjgeo4AA0aeUZvTyamzm8CmJw4icIMv+Jf2b+GUMl9bwLH\n"
-    "7TnMEdW40Lf/7otPBzP57bHGvEg+vkJQjxkK3ZaMTFOL70NxfIst66NhAoGAHWI0\n"
-    "SOwCAsAG64QPa2n2bVEx7Lh2lTghDaBqtlT0qI46JhK7PWX8OSePiFsmXG9tNd9T\n"
-    "6CyaGtM0dVLVfD0JaApKW1zQ3XuIs0upZ/q9cSmmTFr8uC/YwwL3YVM1LQvNj3PV\n"
-    "BpM9L94XvQBqnzBneavaIWhppFDh9as8sZJ71X0CgYEAzB0IiEofAcMraS6ABrrI\n"
-    "cLaYrk9XrD8WabvJTdaukHogOSCq67IpyRfdVUFq6eX/ArMe0GrEbiv67gjL/ejk\n"
-    "73WhCpWKC+Z5oHiAqXtgn0ImOruxAJVFN7760UsRsGvQGCfNFNJ+rmMbv9zkX0i1\n"
-    "FiNxrmVQdgx2lkVZqGhrQJk=\n",
-    "-----END PRIVATE KEY-----\n"
-]
-
-# Склеиваем список строк в монолитный PEM-ключ
-CLEAN_PRIVATE_KEY = "".join(KEY_LINES)
-
-# Авторизационные данные вашего сервисного аккаунта проекта skrp-507012
-GOOGLE_CREDS = {
-    "type": "service_account",
-    "project_id": "skrp-507012",
-    "private_key_id": "c190eb31396cca4f23c87e6f975256eb3fa4432a",
-    "private_key": CLEAN_PRIVATE_KEY,
-    "client_email": "vnyk-468@://gserviceaccount.com",
-    "client_id": "111272482503409086216",
-    "auth_uri": "https://google.com",
-    "token_uri": "https://googleapis.com",
-    "auth_provider_x509_cert_url": "https://googleapis.com",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/vnyk-468%40://gserviceaccount.com",
-    "universe_domain": "googleapis.com",
-}
-
 scope = [
     "https://googleapis.com",
     "https://googleapis.com",
 ]
 
 def get_gspread_client():
-    creds = Credentials.from_service_account_info(GOOGLE_CREDS, scopes=scope)
+    # Программа берет чистые системные данные из панели Secrets
+    if "gcp_service_account" in st.secrets:
+        creds_dict = dict(st.secrets["gcp_service_account"])
+        creds = Credentials.from_service_account_info(creds_dict, scopes=scope)
+    else:
+        st.error("Системные ключи Google API не найдены в настройках Secrets!")
+        st.stop()
     return gspread.authorize(creds)
 
 st.set_page_config(
